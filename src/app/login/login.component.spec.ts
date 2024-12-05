8dc58afea9e083e4f6f74a25430df794e719f3ef
Stevan Lim <stevalim123@gmail.com>
2024-12-05 09:59:21 +0700

    sss

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +8,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent, RouterModule]
     })
     .compileComponents();
 
